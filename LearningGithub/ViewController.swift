de89d2644f97e11a9633301c3cb48372eb948570
Yancy Johnson <johns713@msu.edu>
2020-01-07 03:17:26 -0500

    Initial Commit

---
+++ LearningGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningGithub
+//
+//  Created by Yancy Johnson on 1/7/20.
+//  Copyright © 2020 Yancy Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
